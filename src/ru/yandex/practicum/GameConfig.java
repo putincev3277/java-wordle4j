204fc7b24b6e6815436bcca222751f72f66067a8
putincev3277 <Putincev3277@yandex.ru>
2026-07-07 10:36:31 +0700

    Соответствовать Checkstyle/SonarQube

--- src/ru/yandex/practicum/GameConfig.java
+++ src/ru/yandex/practicum/GameConfig.java
@@ -2,7 +2,9 @@ package ru.yandex.practicum;
 
 public final class GameConfig {
 
-    private GameConfig() {}
+    private GameConfig() {
+
+    }
 
     public static final int WORD_LENGTH = 5;
     public static final int MAX_STEPS = 6;
